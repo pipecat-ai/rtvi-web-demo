e91c130560930582ac02247ac9ee8e6e25a72032
Jon Taylor <heyjtaylor@gmail.com>
2024-07-22 23:15:24 +0100

    remove header deps

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,8 +1,4 @@
-import Logo from "../logo";
 import ExpiryTimer from "../Session/ExpiryTimer";
-
-const aCx =
-  "underline decoration-primary-400/0 hover:decoration-primary-400 underline-offset-4 transition-all duration-300";
 
 function Header() {
   return (
